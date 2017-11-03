a05a5ce0d6fdf3f8f2cd495887c8599d3cae758d
Kathy Satterlee <k.j.satterlee@gmail.com>
2017-11-02 21:02:04 -0400

    completed server-side code

--- server.js
+++ server.js
@@ -6,41 +6,41 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 //set up data parsing
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Data
 const reservations = [];
-const waitlist = [];
 
 function Reservation(name, phone, email, id) {
-  this.name = name;
-  this.phone = phone;
-  this.email = email;
-  this.id = id;
+    this.name = name;
+    this.phone = phone;
+    this.email = email;
+    this.id = id;
 }
 //landing page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
-//add res
-app.get("/reserve", function(req, res) {
-  res.sendFile(path.join(__dirname, "makeReserve.html"));
+//home
+app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
 });
-//add res
-app.get("/reserve", function(req, res) {
-  res.sendFile(path.join(__dirname, "makeReserve.html"));
+//view tables
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "viewTables.html"));
+});
+//enter reservation info
+app.get("/new", function(req, res) {
+    res.sendFile(path.join(__dirname, "makeReserv.html"));
 });
 //Add new reservation
-app.post("/api/new", function(req, res) {
-  let newReservation = new Reservation(req.body);
-  if (reservations.length < 5) reservations.push(newReservation);
-  else waitlist.push(newReservation);
+app.post("/api/new", function (req, res) {
+    let newReservation = new Reservation(req.body);
+    reservations.push(newReservation);
 });
 
 //start server
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
